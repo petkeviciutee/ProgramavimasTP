e6879848539ecc0094fff4bdea8b141b26cb9c43
petkeviciutee <90761629+petkeviciutee@users.noreply.github.com>
2021-09-24 10:59:29 +0300

    Update v02.cpp

--- v02.cpp
+++ v02.cpp
@@ -16,26 +16,17 @@ struct studentas {
     int egz;
     float Galutinisvid;
 };
-void read_from_file(vector<studentas>& grupe, int* pazymiu_sk);
+
 double Mediana(vector <int> vec);
-void print_student(vector<studentas> grupe, int pazymiu_sk);
 unsigned int countWordsInString(string const& str);
 
 
 int main()
 {
     int pazymiu_sk;
-    char temp;
     vector<studentas> grupe;
-    read_from_file(grupe, &pazymiu_sk);
-    print_student(grupe, pazymiu_sk);
-  
+   
 
-return 0;
-}
-//Nuskaitymo funkcija
-void read_from_file(vector<studentas>& grupe, int* pazymiu_sk)
-{
     int student_counter = 0;
     int temp;
     ifstream fileRead;
@@ -44,7 +35,7 @@ void read_from_file(vector<studentas>& grupe, int* pazymiu_sk)
     if (fileRead.is_open())
     {
         getline(fileRead >> ws, buff);
-        *pazymiu_sk = countWordsInString(buff) - 3;
+        pazymiu_sk = countWordsInString(buff) - 3;
         while (true)
         {
 
@@ -52,20 +43,35 @@ void read_from_file(vector<studentas>& grupe, int* pazymiu_sk)
             fileRead >> grupe.at(student_counter).Vardas;
             if (fileRead.eof()) {grupe.pop_back(); break; }
             fileRead >> grupe.at(student_counter).Pavarde;
-            for (int i = 0; i < *pazymiu_sk; i++)
+            for (int i = 0; i < pazymiu_sk; i++)
             {
                 fileRead >> temp;
                 grupe.at(student_counter).paz.push_back(temp);
             }
             fileRead >> grupe.at(student_counter).egz;
           
-            grupe.at(student_counter).Galutinisvid = grupe.at(student_counter).Galutinisvid / *pazymiu_sk;
+            grupe.at(student_counter).Galutinisvid = grupe.at(student_counter).Galutinisvid / pazymiu_sk;
             grupe.at(student_counter).Galutinisvid = grupe.at(student_counter).Galutinisvid * 0.4 + 0.6 * grupe.at(student_counter).egz;
             student_counter++;
         }
     }
     else {cout << "-\n"; }
+
+  
+ofstream output;
+    output.open("rezultatai.txt");
+    output << setw(20) << left << "Vardas" << setw(20) << left << "Pavarde" << setw(18) << left << "Galutinis(vid.)/" << left << "Galutinis(med.)\n"
+      << "--------------------------------------------------------------------------\n";
+
+
+    for (int i = 0; i < grupe.size(); i++)
+    {
+        output << setw(20) << left << grupe[i].Vardas << setw(20) << left << grupe[i].Pavarde << setw(18) << left << grupe[i].Galutinisvid << Mediana(grupe[i].paz) << endl;
+    }
+    output << "\n\n";
+return 0;
 }
+//funkcijos
 unsigned int countWordsInString(string const& str)
 {
     stringstream stream(str);
@@ -81,19 +87,4 @@ double Mediana(vector <int> vec){
   sort(vec.begin(),vec.end());
   vecSize vid = size/2;
   return size % 2 == 0 ? (vec[vid] + vec[vid-1]) / 2 : vec[vid];
-}
-
-void print_student(vector<studentas> grupe, int pazymiu_sk) //atspausdina rezultatus
-{
-    ofstream output;
-    output.open("rezultatai.txt");
-    output << setw(20) << left << "Vardas" << setw(20) << left << "Pavarde" << setw(18) << left << "Galutinis(vid.)/" << left << "Galutinis(med.)\n"
-      << "--------------------------------------------------------------------------\n";
-
-
-    for (int i = 0; i < grupe.size(); i++)
-    {
-        output << setw(20) << left << grupe[i].Vardas << setw(20) << left << grupe[i].Pavarde << setw(18) << left << grupe[i].Galutinisvid << Mediana(grupe[i].paz) << endl;
-    }
-    output << "\n\n";
 }
